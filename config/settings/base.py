6f0928ce6fe6b6d0aa8b09798afa8f05553c2746
yaxshioken <yaxshioken@gmail.com>
2024-10-08 16:48:30 +0500

    fix:all;

--- config/settings/base.py
+++ config/settings/base.py
@@ -19,7 +19,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
-ALLOWED_HOSTS = ["192.168.1.10"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
