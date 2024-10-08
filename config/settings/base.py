820e912546918f64e35a39d5ba9a229c9c195c6d
yaxshioken <yaxshioken@gmail.com>
2024-10-08 15:12:17 +0500

    fix:base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -133,7 +133,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
